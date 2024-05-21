c7229fa508f5514823bcd955f0e0ac7947f5f693
Hassan ul Haq <127566251+HassanKashif23@users.noreply.github.com>
2024-05-21 23:05:24 +0500

    Create 3to8decoder.v

---
+++ Decoder/3to8decoder.v
@@ -0,0 +1,15 @@
+module theDecoder(
+	input [2:0]data,
+	output [7:0]out
+	);
+	
+assign out[0] = (~data[0]&~data[1]&~data[2]);
+assign out[1] = (~data[0]&~data[1]&data[2]);
+assign out[2] = (~data[0]&data[1]&~data[2]);
+assign out[3] = (~data[0]&data[1]&data[2]);
+assign out[4] = (data[0]&~data[1]&~data[2]);
+assign out[5] = (data[0]&~data[1]&data[2]);
+assign out[6] = (data[0]&data[1]&~data[2]);
+assign out[7] = (data[0]&data[1]&data[2]);
+
+endmodule
